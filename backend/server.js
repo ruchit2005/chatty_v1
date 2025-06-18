2e5be54fed3c86ed37a21cc71764b3407883d50f
ruchit2005 <ruchitgupta05@gmail.com>
2025-06-18 19:08:04 +0530

    Changed / path

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,29 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('JS middleware is running.');
+});
+
 
 app.post('/chat', async (req, res) => {
   try {
     const { message } = req.body;
+    console.log("JS Received:", message);
 
-    const pythonResponse = await axios.post('https://chatty-backend-production-4b11.up.railway.app/chat', { message });
+    const pythonResponse = await axios.post(
+      'https://chatty-backend-production-4b11.up.railway.app/chat',
+      { message }
+    );
 
-
-
+    console.log("Python replied:", pythonResponse.data.response);
     res.json({ response: pythonResponse.data.response });
   } catch (error) {
-    console.error(error);
+    console.error("Error in JS backend:", error.message);
     res.status(500).json({ error: 'Failed to get response from Python backend' });
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Node.js server running on port ${PORT}`);
